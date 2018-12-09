b49c59fd75f61f88c418d37d8b390dd98603d262
RPMorganomous <mba.rick@gmail.com>
2018-12-09 17:45:09 -0600

    what devan loves

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApplication1
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("devan should play with me");
+			Console.WriteLine("devan loves c#");
 			
 			StreamReader myReader = new StreamReader("D:\\developement\\Advent2018\\Day01\\files\\day01input.txt");
 			string line = "";
